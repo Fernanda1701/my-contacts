9781383fa77bb21ef854c312184dcca247eb053e
Fernanda Nascimento <74015474+Fernanda1701@users.noreply.github.com>
2022-07-16 13:30:35 -0300

    App files

---
+++ src/components/Details.jsx
@@ -0,0 +1,9 @@
+
+function Details(props){
+    return <div className="bottom">
+    <p className="info">{props.tel}</p>
+    <p className="info">{props.email}</p>
+  </div>
+}
+
+export default Details;
